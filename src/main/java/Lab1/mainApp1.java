d13df300fd4fc29f203b01012a21804d48ba3a72
meng <gordonmeng2021@gmail.com>
2023-09-14 17:18:38 +0800

    adding first print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,8 +5,8 @@ public class mainApp1 {
         int b=2;
         int n=11;
         System.out.println(b+"to the power"+n+" = "+ myLibrary.Power(b,n));
-        System.out.println(n+"! = "+myLibrary.factorial(n));
-        System.out.println("Program ended");
+//        System.out.println(n+"! = "+myLibrary.factorial(n));
+//        System.out.println("Program ended");
 
     }
 }
